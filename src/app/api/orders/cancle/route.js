c0ce079edddc57563060ab2e3a840fc0ec72a7fa
LinkPlayer047 <linkplayer047@gmail.com>
2025-12-17 17:11:51 +0500

    Ok

--- src/app/api/orders/cancle/route.js
+++ src/app/api/orders/cancle/route.js
@@ -1,6 +1,6 @@
 import connectToDB from "@/lib/db";
 import { Order } from "@/models/order";
-import { getUserFromToken } from "@/lib/auth";
+import { getUserFromToken } from "@/lib/adminauth";
 import { corsHeaders } from "@/lib/cors";
 
 export async function PATCH(req) {
--- src/lib/adminauth.js
+++ src/lib/adminauth.js
@@ -4,7 +4,9 @@ import User from "@/models/user";
 
 export async function getUserFromToken(token) {
   if (!token) return null;
+
   await connectToDB();
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     return await User.findById(decoded.id);
