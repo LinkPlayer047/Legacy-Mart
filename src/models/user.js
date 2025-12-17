21fd8071d05cb05ea4885d5138090c9063170c08
LinkPlayer047 <linkplayer047@gmail.com>
2025-12-17 13:55:08 +0500

    ok

--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   role: { type: String, enum: ["user","admin"], default: "user" },
   status: { type: String, enum: ["active","blocked"], default: "active" },
   verified: { type: Boolean, default: false },
-  otp: Number,
+  otp: { type: Number }
 }, { timestamps: true });
 
 const User = mongoose.models.User || mongoose.model("User", UserSchema);
-export default User; // ✅ default export
+
+export default User;
